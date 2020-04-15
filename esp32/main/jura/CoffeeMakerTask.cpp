4e1d5459eaa4cf7c5d1154d887f0f1e6e467e7da
Fabian Sauter <sauter.fabian@mailbox.org>
2020-04-15 15:31:35 +0200

    Fixed the serial connection and added basic JURA encoding

--- esp32/main/jura/CoffeeMakerTask.cpp
+++ esp32/main/jura/CoffeeMakerTask.cpp
@@ -1,40 +1,101 @@
 #include "CoffeeMakerTask.hpp"
 
-#include <smooth/core/task_priorities.h>
-
 #include <chrono>
 #include <iostream>
+#include <string>
 
-#include "driver/gpio.h"
+#include "smooth/core/task_priorities.h"
 
 //---------------------------------------------------------------------------
 namespace esp32jura::jura {
 //---------------------------------------------------------------------------
-CoffeeMakerTask::CoffeeMakerTask()
-    : Task("Coffee Task", 0, 1, std::chrono::seconds(1), 1) {}
+CoffeeMakerTask::CoffeeMakerTask() : Task("Coffee Task", 0, 1, std::chrono::seconds(1), 1) {}
 
 void CoffeeMakerTask::init() {
     std::cout << "Initializing coffee maker...\n";
-    uart_set_pin(UART_NUM_1, GPIO_NUM_4, GPIO_NUM_5, UART_PIN_NO_CHANGE,
-                 UART_PIN_NO_CHANGE);
-    uart_driver_install(UART_NUM_1, BUF_SIZE * 2, 0, 0, NULL, 0);
-    uart_param_config(UART_NUM_1, &uartConfig);
+    ESP_ERROR_CHECK(uart_param_config(UART_PORT, &uartConfig));
+
+    // Set UART pins(TX: IO16 (UART2 default), RX: IO17 (UART2 default), RTS:
+    // IO18, CTS: IO19)
+    ESP_ERROR_CHECK(uart_set_pin(UART_PORT, UART_TX, UART_RX, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE));
+    // Install UART driver using an event queue here
+    ESP_ERROR_CHECK(uart_driver_install(UART_PORT, BUF_SIZE * 2, 0, 0, nullptr, 0));
     std::cout << "Coffee maker initialized.\n";
 }
 
 void CoffeeMakerTask::tick() {
     std::cout << "Coffee maker tick...\n";
-    uint8_t buf[BUF_SIZE];
-    int len = 0;
     while (true) {
-        len = uart_read_bytes(UART_NUM_1, buf, BUF_SIZE, 20 / portTICK_RATE_MS);
-        if (len < 0) {
-            std::cerr << "Failed to read from UART." << std::endl;
-        } else if (len > 0) {
-            std::cout << "Read: " << len << " bytes\n";
-        }
+        uartWriteTest();
+        uartReadTest();
     }
 }
+
+void CoffeeMakerTask::uartWriteTest() {
+    std::string s = "Hallo";
+    int len = uart_write_bytes(UART_PORT, s.c_str(), s.length());
+}
+
+void CoffeeMakerTask::uartReadTest() {
+    size_t size = 0;
+    ESP_ERROR_CHECK(uart_get_buffered_data_len(UART_PORT, &size));
+    if (size <= 0) {
+        return;
+    }
+
+    uint8_t data[128];
+    int len = uart_read_bytes(UART_PORT, data, size, 100);
+    if (len < 0) {
+        std::cerr << "Failed to read from UART." << std::endl;
+    } else if (len > 0) {
+        std::cout << "Read: " << len << " bytes\n";
+    }
+}
+
+uint8_t CoffeeMakerTask::decode(std::array<uint8_t, 4> encData) {
+    // Bit mask for the 2. bit from the left:
+    constexpr uint8_t B2_MASK = (0b10000000 >> 2);
+    // Bit mask for the 5. bit from the left:
+    constexpr uint8_t B5_MASK = (0b10000000 >> 5);
+
+    uint8_t decData = 0;
+    decData |= (encData[0] & B2_MASK) << 2;
+    decData |= (encData[0] & B5_MASK) << 4;
+
+    decData |= (encData[1] & B2_MASK);
+    decData |= (encData[1] & B5_MASK) << 2;
+
+    decData |= (encData[2] & B2_MASK) >> 2;
+    decData |= (encData[2] & B5_MASK);
+
+    decData |= (encData[3] & B2_MASK) >> 4;
+    decData |= (encData[3] & B5_MASK) << 2;
+    return decData;
+}
+
+std::array<uint8_t, 4> CoffeeMakerTask::encode(uint8_t decData) {
+    // The base bit layout for all send bytes:
+    constexpr uint8_t BASE = 0b01111111;
+
+    std::array<uint8_t, 4> encData{0, 0, 0, 0};
+    encData[0] = BASE & ((decData & 0b10000000) >> 2);
+    encData[0] = BASE & ((decData & 0b01000000) >> 4);
+
+    encData[1] = BASE & (decData & 0b00100000);
+    encData[1] = BASE & ((decData & 0b00010000) >> 1);
+
+    encData[2] = BASE & ((decData & 0b00001000) << 2);
+    encData[2] = BASE & (decData & 0b00000100);
+
+    encData[3] = BASE & ((decData & 0b00000010) << 4);
+    encData[3] = BASE & ((decData & 0b00000001) << 2);
+    return std::move(encData);
+}
+
+void CoffeeMakerTask::writeEncData(std::array<uint8_t, 4> encData) { uart_write_bytes(UART_PORT, reinterpret_cast<char*>(encData.data()), 4); }
+
+std::array<uint8_t, 4> CoffeeMakerTask::readEncData() { return std::array<uint8_t, 4>{0, 0, 0, 0}; }
+
 //---------------------------------------------------------------------------
 }  // namespace esp32jura::jura
 //---------------------------------------------------------------------------
--- esp32/main/jura/CoffeeMakerTask.hpp
+++ esp32/main/jura/CoffeeMakerTask.hpp
@@ -1,26 +1,56 @@
 #pragma once
 
-#include <smooth/core/Task.h>
+#include <array>
 
+#include "driver/gpio.h"
 #include "driver/uart.h"
+#include "smooth/core/Task.h"
 
 //---------------------------------------------------------------------------
 namespace esp32jura::jura {
 //---------------------------------------------------------------------------
 class CoffeeMakerTask : public smooth::core::Task {
    private:
-    uart_config_t uartConfig{.baud_rate = 9600,
-                             .data_bits = UART_DATA_8_BITS,
-                             .parity = UART_PARITY_DISABLE,
-                             .stop_bits = UART_STOP_BITS_1,
-                             .flow_ctrl = UART_HW_FLOWCTRL_DISABLE};
+    uart_config_t uartConfig{
+        .baud_rate = 9600,
+        .data_bits = UART_DATA_8_BITS,
+        .parity = UART_PARITY_DISABLE,
+        .stop_bits = UART_STOP_BITS_1,
+        .flow_ctrl = UART_HW_FLOWCTRL_DISABLE,
+    };
     static constexpr int BUF_SIZE = 1024;
+    static constexpr uart_port_t UART_PORT = UART_NUM_1;
+    static constexpr gpio_num_t UART_TX = GPIO_NUM_4;
+    static constexpr gpio_num_t UART_RX = GPIO_NUM_5;
 
    public:
     CoffeeMakerTask();
 
     void init() override;
     void tick() override;
+
+   private:
+    void uartWriteTest();
+    void uartReadTest();
+
+    /**
+     * Decodes the given four bytes read from the coffee maker into on byte.
+     * Based on: http://protocoljura.wiki-site.com/index.php/Protocol_to_coffeemaker
+     **/
+    uint8_t decode(std::array<uint8_t, 4> encData);
+    /**
+     * Encodes the given byte into four bytes that the coffee maker understands.
+     * Based on: http://protocoljura.wiki-site.com/index.php/Protocol_to_coffeemaker
+     **/
+    std::array<uint8_t, 4> encode(uint8_t decData);
+    /**
+     * Writes four bytes of data to the coffee maker and then waits 8ms.
+     **/
+    void writeEncData(std::array<uint8_t, 4> encData);
+    /**
+     * Reads four bytes of data from the coffee maker which represent one byte of actual data.
+     **/
+    std::array<uint8_t, 4> readEncData();
 };
 //---------------------------------------------------------------------------
 }  // namespace esp32jura::jura
