d022845e34cf98123213d8d5d3edd6426d4a35b3
Fabian Sauter <sauter.fabian@mailbox.org>
2020-08-07 14:52:45 +0200

    Fixed serial connection stack overflow

--- esp32/main/serial/SerialConnection.cpp
+++ esp32/main/serial/SerialConnection.cpp
@@ -8,10 +8,10 @@
 //---------------------------------------------------------------------------
 namespace esp32jura::serial {
 //---------------------------------------------------------------------------
-SerialConnection::SerialConnection() : Task("Serial Task", 0, 1, std::chrono::milliseconds(100), 1) {}
+SerialConnection::SerialConnection() : Task("Serial Task", 0, smooth::core::APPLICATION_BASE_PRIO, std::chrono::milliseconds(100), 1) { read_buffer.reserve(BUF_SIZE); }
 
 void SerialConnection::init() {
-    assert(state != SC_DISABLED && state != SC_ERROR);
+    assert(state == SC_DISABLED || state == SC_ERROR);
     std::cout << "Initializing serial connection on port (Port: " << UART_PORT << " , TX: " << UART_TX << " , RX: " << UART_RX << ")...\n";
     ESP_ERROR_CHECK(uart_param_config(UART_PORT, &uartConfig));
     ESP_ERROR_CHECK(uart_set_pin(UART_PORT, UART_TX, UART_RX, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE));
@@ -22,32 +22,54 @@ void SerialConnection::init() {
 
 void SerialConnection::tick() {
     assert(state == SC_READY);
-    while (true) {
-        std::string s = read();
-        s = "Thanks: " + s;
+    std::string s = read();
+    if (!s.empty()) {
+        s = "Thanks: " + s + "\r\n";
         write(s);
     }
 }
 
-std::string SerialConnection::read() {
+bool SerialConnection::read_byte() {
+    uint8_t c;
+    int len = uart_read_bytes(UART_PORT, &c, 1, 1000 / portTICK_PERIOD_MS);
+    if (len <= 0) {
+        std::cerr << "Failed to read from UART." << std::endl;
+        return false;
+    }
+    if (c != '\n' && c != '\r') {
+        read_buffer.push_back(c);
+        return false;
+    }
+    return true;
+}
+
+bool SerialConnection::data_available() {
     size_t size = 0;
     ESP_ERROR_CHECK(uart_get_buffered_data_len(UART_PORT, &size));
-    if (size < 4) {
-        return "";
-    }
+    return size > 0;
+}
 
-    // Wait 8ms for the data to arrive + 4ms for fail save reasons:
-    int len = uart_read_bytes(UART_PORT, read_buffer.data(), read_buffer.size(), (8 + 4) / portTICK_PERIOD_MS);
-    if (len < 0) {
-        std::cerr << "Failed to read from UART." << std::endl;
-        return "";
-    }
-
-    std::ostringstream convert;
-    for (size_t i = 0; i < len; i++) {
-        convert << (int)read_buffer[i];
+std::string SerialConnection::read() {
+    while (data_available()) {
+        if (read_byte()) {
+            std::string result = vec_to_string(read_buffer);
+            read_buffer.clear();
+            return result;
+        }
     }
     return "";
+}
+
+std::string SerialConnection::vec_to_string(const std::vector<uint8_t>& data) {
+    if (data.size() <= 0) {
+        return "";
+    }
+
+    std::ostringstream sstream;
+    for (size_t i = 0; i < data.size(); i++) {
+        sstream << static_cast<char>(data[i]);
+    }
+    return sstream.str();
 }
 
 size_t SerialConnection::write(const std::vector<uint8_t>& data) {
--- esp32/main/serial/SerialConnection.hpp
+++ esp32/main/serial/SerialConnection.hpp
@@ -1,6 +1,5 @@
 #pragma once
 
-#include <array>
 #include <string>
 #include <vector>
 
@@ -25,11 +24,15 @@ class SerialConnection : public smooth::core::Task {
         .rx_flow_ctrl_thresh = 122,
         .use_ref_tick = false,
     };
-    static constexpr int BUF_SIZE = 2048;
     static constexpr uart_port_t UART_PORT = UART_NUM_0;
     static constexpr gpio_num_t UART_TX = GPIO_NUM_1;
     static constexpr gpio_num_t UART_RX = GPIO_NUM_3;
-    std::array<uint8_t, BUF_SIZE> read_buffer{};
+    static constexpr int BUF_SIZE = 2048;
+    /**
+     * Use std::vector instead of std::array to save on stack space.
+     * std::array allocates memory on the stack.
+     **/
+    std::vector<uint8_t> read_buffer;
 
     SerialConnectionState state{SC_DISABLED};
 
@@ -41,8 +44,15 @@ class SerialConnection : public smooth::core::Task {
 
    private:
     std::string read();
+    /**
+     * Reads a single byte from the uart buffer and appends it to the "read_buffer".
+     * Returns true if the byte is a new line character ('\n').
+     **/
+    bool read_byte();
+    bool data_available();
     size_t write(const std::vector<uint8_t>& data);
     size_t write(const std::string& data);
+    static std::string vec_to_string(const std::vector<uint8_t>& data);
 };
 //---------------------------------------------------------------------------
 }  // namespace esp32jura::serial
