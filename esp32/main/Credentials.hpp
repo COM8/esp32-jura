b2532e76f1ce549852fe37793e2ab5c7b7859a6f
Fabian Sauter <sauter.fabian@mailbox.org>
2020-08-11 09:55:30 +0200

    Updated the placeholder credential description

--- esp32/main/Credentials.hpp
+++ esp32/main/Credentials.hpp
@@ -5,6 +5,13 @@
 //---------------------------------------------------------------------------
 namespace esp32jura {
 //---------------------------------------------------------------------------
+/**
+ * This file contains credentials for debugging the XMPP and WIFI connection,
+ * without having to go through the usual setup procedure.
+ * To apply these settings uncomment the "initWithDummyValues()" call in the "Esp32Jura.cpp" file.
+ *
+ * !!NEVER COMMIT THIS FILE!!
+ **/
 const std::string SSID = "Your WIFI SSID";
 const std::string PASSWORD = "Your WIFI Password";
 const std::string JID = "JID of this device";
