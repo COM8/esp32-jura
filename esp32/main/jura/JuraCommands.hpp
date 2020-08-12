7781366909c29cd10633c6eb42491eb74db65e9e
Fabian Sauter <sauter.fabian@mailbox.org>
2020-08-12 09:16:37 +0200

    Fixed JURA commands

--- esp32/main/jura/JuraCommands.hpp
+++ esp32/main/jura/JuraCommands.hpp
@@ -5,18 +5,18 @@
 //---------------------------------------------------------------------------
 namespace esp32jura::jura {
 //---------------------------------------------------------------------------
-const std::string JURA_POWER_OFF = "AN:01";
-const std::string JURA_TEST_MODE_ON = "AN:20";
-const std::string JURA_TEST_MODE_OFF = "AN:21";
+const std::string JURA_POWER_OFF = "AN:01\r\n";
+const std::string JURA_TEST_MODE_ON = "AN:20\r\n";
+const std::string JURA_TEST_MODE_OFF = "AN:21\r\n";
 
-const std::string JURA_GET_TYPE = "TY:";
+const std::string JURA_GET_TYPE = "TY:\r\n";
 
-const std::string JURA_BUTTON_1 = "FA:04";
-const std::string JURA_BUTTON_2 = "FA:05";
-const std::string JURA_BUTTON_3 = "FA:06";
-const std::string JURA_BUTTON_4 = "FA:07";
-const std::string JURA_BUTTON_5 = "FA:08";
-const std::string JURA_BUTTON_6 = "FA:09";
+const std::string JURA_BUTTON_1 = "FA:04\r\n";
+const std::string JURA_BUTTON_2 = "FA:05\r\n";
+const std::string JURA_BUTTON_3 = "FA:06\r\n";
+const std::string JURA_BUTTON_4 = "FA:07\r\n";
+const std::string JURA_BUTTON_5 = "FA:08\r\n";
+const std::string JURA_BUTTON_6 = "FA:09\r\n";
 //---------------------------------------------------------------------------
 }  // namespace esp32jura::jura
 //---------------------------------------------------------------------------
